cea0775fc0b892247bbf36bb488ca8e7e7fba6c1
Toqi Abdullah <toqiabdullah61990@gmail.com>
2025-09-23 00:49:44 +0600

    Setup in MVC Structure with config.js & helper.js file Done

---
+++ Forkify-Fullstack-Project/src/js/model.js
@@ -0,0 +1,27 @@
+import "regenerator-runtime/runtime.js";
+import { API_URL } from "./config";
+import { getJson } from "./helpers";
+
+export const state = {
+    recipe: {}
+};
+
+export const loadRecipe = async function (id){
+    try {
+        const data = await getJson(`${API_URL}/${id}`)
+            let {recipe} = data.data;
+            state.recipe = {
+                id: recipe.id,
+                title: recipe.title,
+                publisher: recipe.publisher,
+                sourceUrl: recipe.source_url,
+                image: recipe.image_url,
+                servings: recipe.servings,
+                cookingTime: recipe.cooking_time,
+                ingredients: recipe.ingredients
+            }
+            console.log(state.recipe)
+    } catch (error) {
+        console.log(error)
+    }
+}
